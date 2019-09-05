14824265b47b603c5ee38056f4098b0c05a66b2e
Nicolae Nichifor <nick@superwpheroes.io>
2019-09-05 14:56:00 +0300

    Expose WooCommerceAPI

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import WooWorker from "./src/WooCommerce/WooWorker";
+import WCAPI from "./src/WooCommerce/WooCommerceAPI";
+
 // import AuthWorker from "./src/Authentication/AuthWorker";
- 
-export { WooWorker };
+
+export { WooWorker, WCAPI };
